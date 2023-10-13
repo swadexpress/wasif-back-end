f16b9d0118f3620400139f9c6a58e61dc7bb15c9
swadexpress <muhammadkawsarkhan@gmail.com>
2023-10-14 00:07:31 +0600

    up

--- incomeexpensesapi/asgi.py
+++ incomeexpensesapi/asgi.py
@@ -5,20 +5,16 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-import os
-import django
-from chat.routing import websocket_urlpatterns
 from channels.routing import get_default_application
-import channels.asgi
+import django
+
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "incomeexpensesapi.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-# django.setup()
-# application = get_default_application()
-channel_layer = channels.asgi.get_channel_layer()
-
-
+django.setup()
+application = get_default_application()
 
 
 
