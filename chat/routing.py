95bcb126e50f7e5146cdebc25927870db41183a4
swadexpress <muhammadkawsarkhan@gmail.com>
2024-09-08 22:44:25 +0600

    u;

--- chat/routing.py
+++ chat/routing.py
@@ -7,8 +7,8 @@ websocket_urlpatterns = [
     re_path(r"ws/fruitgame/(?P<room_name>\w+)/$", consumers.FruitgameConsumer.as_asgi()),
     re_path(r"ws/fruit-loop-game/(?P<room_name>\w+)/$", consumers.FruitLoopgameConsumer.as_asgi()),
     re_path(r"ws/rocket-crash-game/(?P<room_name>\w+)/$", consumers.RocketCrashConsumer.as_asgi()),
+    re_path(r"ws/slot-machine-game/(?P<room_name>\w+)/$", consumers.SlotMachineGame.as_asgi()),
     re_path(r"ws/p2p-messages/(?P<room_name>\w+)/$", consumers.P2PMessages.as_asgi()),
-
-
+    re_path(r"ws/super-lottery-game/(?P<room_name>\w+)/$", consumers.SuperLotteryGame.as_asgi()),
 
 ]
